9782b8db2a7149ba3e0d6cf92d320722755c0754
Reece Mercer <reecemercer@outlook.com>
2019-04-03 13:40:05 +0100

    Think I found the source of the app crashes

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-/*
-const http = require('http');
-const middleware = require('./middlewareRouter');
-
-
-// hardcoded port for dev purposes
-// when the app is being hosted this would be different
-const port = process.env.PORT || 80;
-
-// bind middleware to http server as a request handler
-const server = http.createServer(middleware);
-
-server.listen(port, () => console.log(`Server up @localhost:${port}`));
-*/
-
 const middleware = require('./middlewareRouter');
 
 const express = require("express");
@@ -46,4 +31,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 })
 */
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log("Server started"));
